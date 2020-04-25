949dccc0eb77b17de5d1dd2f3db8c5ed26a33ec2
maximilinyh <maxim_ilinyh@mail.ru>
2020-04-25 19:17:36 +0700

    25/04 commit

--- src/Redux/filterItemsReducer.js
+++ src/Redux/filterItemsReducer.js
@@ -7,7 +7,7 @@ const initialState = {
 export const filterItemsReducer = (state = initialState, action) => {
     switch (action.type) {
         case SET_FILTER:
-            return {filter: action.payload}
+            return {...state, filter: action.payload}
         default:
             return state
     }
